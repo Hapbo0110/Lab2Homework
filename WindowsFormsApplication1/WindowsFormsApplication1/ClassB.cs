e6b43d287f04a96f5e268dabc20ce86568c4017a
Hapbo <33798903+Hapbo@users.noreply.github.com>
2017-11-24 21:40:02 +0800

    Update ClassB.cs

--- WindowsFormsApplication1/WindowsFormsApplication1/ClassB.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/ClassB.cs
@@ -8,6 +8,18 @@ namespace WindowsFormsApplication1
 {
     class ClassB
     {
+        bool check;
+        public bool Check(int n)
+        {
+            check = true;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) check = false;
+            }
 
+
+
+                return check;
+        }
     }
 }
